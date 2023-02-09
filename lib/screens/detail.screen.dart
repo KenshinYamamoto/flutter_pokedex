c0b8577687b96bd1b50ece9e7870467036ef2787
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-02-09 17:39:43 +0900

    DetailScreenの表示内容を変更・イラストの取得先の変更

--- lib/screens/detail.screen.dart
+++ lib/screens/detail.screen.dart
@@ -6,12 +6,55 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as Map;
+    // print(args);
+    final String sprite = args['sprite'];
+    final String name = args['name'];
+    final List<String> types = args['types'];
+    print(sprite);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('detail'),
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.center,
+          child: FittedBox(
+            child: Column(
+              children: [
+                Image.network(
+                  sprite,
+                  scale: 0.3,
+                ),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    for (int i = 0; i < types.length; i++) ...{
+                      Text(
+                        types[i],
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (i != types.length - 1) ...{
+                        const Text('・'),
+                      },
+                    },
+                    const Text('タイプ'),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/screens/pokedex_list_screen.dart
+++ lib/screens/pokedex_list_screen.dart
@@ -13,12 +13,20 @@ class PokedexList extends StatefulWidget {
 
 class _PokedexListState extends State<PokedexList> {
   final Dio dio = Dio();
+  // ポケモン個別のURLを入れる
   List urls = [];
+  // ポケモン個別のデータ
   List indivisualDatas = [];
+  // ポケモン個別のspeciesData
   List speciesDatas = [];
+  // 前のページ
   String? previousUrl = '';
+  // 後のページ
   String? nextUrl = '';
 
+  bool isLoading = true;
+
+  // ポケモン個別のURLをフェッチする
   Future<void> fetchUrls(String url) async {
     final Response<dynamic> response = await dio.get(url);
     previousUrl = response.data['previous'];
@@ -27,6 +35,7 @@ class _PokedexListState extends State<PokedexList> {
       urls.clear();
       indivisualDatas.clear();
       speciesDatas.clear();
+      isLoading = true;
     });
     for (int i = 0; i < response.data['results'].length; i++) {
       urls.add(response.data['results'][i]['url']);
@@ -34,6 +43,7 @@ class _PokedexListState extends State<PokedexList> {
     fetchIndividualDatas();
   }
 
+  // 色々フェッチする
   Future<void> fetchIndividualDatas() async {
     for (int i = 0; i < urls.length; i++) {
       final fetchIndividualFiles = await dio.get(urls[i]);
@@ -43,6 +53,7 @@ class _PokedexListState extends State<PokedexList> {
       );
       speciesDatas.add(speciesData.data);
     }
+    isLoading = false;
     setState(() {});
     print(urls);
     print(indivisualDatas);
@@ -64,91 +75,151 @@ class _PokedexListState extends State<PokedexList> {
         title: const Text('Pokedex'),
       ),
       backgroundColor: Colors.pinkAccent,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1 / 1,
-              ),
-              itemCount: urls.length,
-              itemBuilder: (context, index) {
-                try {
-                  return Card(
-                    elevation: 5,
-                    child: InkWell(
-                      onTap: () => Navigator.of(context).pushNamed(DetailScreen.routeName),
-                      child: FittedBox(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Image.network(
-                              indivisualDatas[index]['sprites']['front_default'],
-                            ),
-                            FittedBox(
-                              child: Text(
-                                speciesDatas[index]['names'][0]['name'],
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1 / 1,
+                      ),
+                      itemCount: urls.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 5,
+                          child: InkWell(
+                            onTap: () async {
+                              final List<String> types = [];
+                              String description = '';
+
+                              // 日本語の説明文を探す
+                              for (int i = 0;
+                                  i <
+                                      speciesDatas[index]['flavor_text_entries']
+                                          .length;
+                                  i++) {
+                                if (speciesDatas[index]['flavor_text_entries']
+                                        [i]['language']['name'] ==
+                                    'ja') {
+                                  description = speciesDatas[index]
+                                      ['flavor_text_entries'][i]['flavor_text'];
+                                }
+                              }
+
+                              // 日本語のタイプを探す
+                              for (int i = 0;
+                                  i < indivisualDatas[index]['types'].length;
+                                  i++) {
+                                final typeDatas = await dio.get(
+                                  indivisualDatas[index]['types'][i]['type']
+                                      ['url'],
+                                );
+                                for (int j = 0;
+                                    j < typeDatas.data['names'].length;
+                                    j++) {
+                                  if (typeDatas.data['names'][j]['language']
+                                          ['name'] ==
+                                      'ja') {
+                                    final type =
+                                        typeDatas.data['names'][j]['name'];
+                                    types.add(type);
+                                  }
+                                }
+                              }
+                              if (!mounted) return;
+                              Navigator.of(context).pushNamed(
+                                DetailScreen.routeName,
+                                arguments: <String, dynamic>{
+                                  // イラスト
+                                  'sprite':
+                                      'https://img.pokemondb.net/artwork/${indivisualDatas[index]['name']}.jpg',
+                                  // 名前
+                                  'name': speciesDatas[index]['names'][0]
+                                      ['name'],
+                                  // タイプ
+                                  'types': types,
+                                  // 何ポケモン
+                                  'kind': speciesDatas[index]['genera'][0]
+                                      ['genus'],
+                                  // 説明文
+                                  'description': description,
+                                },
+                              );
+                            },
+                            child: FittedBox(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Image.network(
+                                    // indivisualDatas[index]['sprites']
+                                    //     ['front_default'],
+                                    'https://img.pokemondb.net/artwork/${indivisualDatas[index]['name']}.jpg',
+                                    height: deviceHeight * 0.1,
+                                    width: deviceWidth * 0.2,
+                                    // scale: 1,
+                                  ),
+                                  Text(
+                                    speciesDatas[index]['names'][0]['name'],
+                                  ),
+                                ],
                               ),
-                            )
-                          ],
-                        ),
-                      ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                } catch (e) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                previousUrl == null
-                    ? SizedBox(
-                        height: deviceHeight * 0.1,
-                        width: deviceWidth * 0.45,
-                      )
-                    : SizedBox(
-                        height: deviceHeight * 0.1,
-                        width: deviceWidth * 0.45,
-                        child: Card(
-                          elevation: 5,
-                          child: InkWell(
-                            onTap: () => fetchUrls(previousUrl!),
-                            child: const FittedBox(
-                              child: Text('前'),
-                            ),
-                          ),
-                        ),
-                      ),
-                nextUrl == null
-                    ? SizedBox(
-                        height: deviceHeight * 0.1,
-                        width: deviceWidth * 0.45,
-                      )
-                    : SizedBox(
-                        height: deviceHeight * 0.1,
-                        width: deviceWidth * 0.45,
-                        child: Card(
-                          elevation: 5,
-                          child: InkWell(
-                            onTap: () => fetchUrls(nextUrl!),
-                            child: const FittedBox(
-                              child: Text('次'),
-                            ),
-                          ),
-                        ),
-                      ),
-              ],
-            )
-          ],
-        ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        previousUrl == null
+                            ? SizedBox(
+                                height: deviceHeight * 0.1,
+                                width: deviceWidth * 0.45,
+                              )
+                            : SizedBox(
+                                height: deviceHeight * 0.1,
+                                width: deviceWidth * 0.45,
+                                child: Card(
+                                  elevation: 5,
+                                  child: InkWell(
+                                    onTap: () => fetchUrls(previousUrl!),
+                                    child: const FittedBox(
+                                      child: Text('前'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                        nextUrl == null
+                            ? SizedBox(
+                                height: deviceHeight * 0.1,
+                                width: deviceWidth * 0.45,
+                              )
+                            : SizedBox(
+                                height: deviceHeight * 0.1,
+                                width: deviceWidth * 0.45,
+                                child: Card(
+                                  elevation: 5,
+                                  child: InkWell(
+                                    onTap: () => fetchUrls(nextUrl!),
+                                    child: const FittedBox(
+                                      child: Text('次'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
       ),
     );
   }
